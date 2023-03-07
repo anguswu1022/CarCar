33e04b90275f6ff47d672fb59a189f53dc3b3e81
Angus Wu <anguswu1022@ucla.edu>
2023-03-06 17:55:37 -0800

    Created Sale_person, customer, sale, and automobileVO models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
+
 # Create your models here.
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    vin = models.CharField(max_length=17, unique=True)
+
+    def __str__(self):
+        return self.vin
+
+class Sales_Person(models.Model):
+    name = models.CharField(max_length=200)
+    employee_number = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+class Sale(models.Model):
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sales",
+        on_delete=models.CASCADE,
+    )
+    sales_person = models.ForeignKey(
+        Sales_Person,
+        related_name="sales",
+        on_delete=models.CASCADE,
+    )
+    customer = models.ForeignKey(
+        Customer,
+        related_name="sales",
+        on_delete=models.CASCADE,
+    )
+    price = models.CharField(max_length=15)
