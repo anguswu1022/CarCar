e13a73e8406d6d97a4b7b80d849c59ef4fb076cb
Angus Wu <anguswu1022@ucla.edu>
2023-03-07 14:26:43 -0800

    Made separate encoders file

---
+++ sales/api/sales_rest/encoders.py
@@ -0,0 +1,43 @@
+from common.json import ModelEncoder
+from .models import AutomobileVO, Sales_Person, Customer, Sale
+
+
+class AutomobileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = [
+        "id",
+        "vin",
+        "sold",
+    ]
+
+class SalesPersonEncoder(ModelEncoder):
+    model = Sales_Person
+    properties = [
+        "id",
+        "name",
+        "employee_number",
+    ]
+
+class CustomerEncoder(ModelEncoder):
+    model = Customer
+    properties = [
+        "id",
+        "name",
+        "address",
+        "phone_number",
+    ]
+
+class SaleEncoder(ModelEncoder):
+    model = Sale
+    properties = [
+        "id",
+        "price",
+        "automobile",
+        "sales_person",
+        "customer",
+    ]
+    encoders = {
+        "automobile": AutomobileVOEncoder(),
+        "sales_person": SalesPersonEncoder(),
+        "customer": CustomerEncoder(),
+    }
