087a83f0772f4b5539f6dbc3a4e2415684ad1e14
Angus Wu <anguswu1022@ucla.edu>
2023-03-06 17:56:50 -0800

    Create and view model objects in admin

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Sale, Sales_Person
+
 
 # Register your models here.
+@admin.register(Customer, Sale, Sales_Person)
+class SalesAdmin(admin.ModelAdmin):
+    pass
