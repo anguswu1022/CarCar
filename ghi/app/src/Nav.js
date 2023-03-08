901007ffe0ca7a9a39b82bb2cf66d08591c5cfa6
Ethan Brown <ethanisbrown@gmail.com>
2023-03-07 20:12:06 -0800

    Completed Techform and updated navbar.

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,6 +10,9 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li>
+            <NavLink className="navbar-brand" to="/technician">New technician</NavLink>
+            </li>
           </ul>
         </div>
       </div>
